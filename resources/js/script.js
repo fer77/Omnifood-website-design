c93e8980b3a5b3ef7dbf91bce244c0b830ee388c
Fernando <ffmp777@yahoo.com>
2016-11-25 22:25:45 -0700

    Added initial scroll to sections in nav

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+    /* Sticky Nav */
     /* var waypoints = $('#handler-first').waypoint(function(direction) {
   notify(this.element.id + ' hit 25% from top of window')
 }, {
@@ -12,5 +13,26 @@ $(document).ready(function() {
         }
     }, {
         offset: '60px;'
+    });
+    /* Scroll on Buttons */
+    $('.js-scroll-plans').click(function() {
+        $('html, body').animate({ scrollTop: $('.js-plans').offset().top }, 2000);
+    });
+
+    $('.js-scroll-features').click(function() {
+        $('html, body').animate({ scrollTop: $('.js-features').offset().top }, 1000);
+    });
+    /* Nav Scroll */
+    $(function() {
+        $('a[href*="#"]:not([href="#"])').click(function() {
+            if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+            if (target.length) {
+                $('html, body').animate({ scrollTop: target.offset().top }, 1500);
+                return false;
+                }
+            }
+        });
     });
 });
